86aed6453590147ad625439253252572136dd977
samirjosue <samirjosue@gmail.com>
2017-08-25 00:17:14 -0300

    testes

--- resources/views/patrimonio/listagem.blade.php
+++ resources/views/patrimonio/listagem.blade.php
@@ -28,18 +28,6 @@
     </button>
 </form>
 
-<div class="container">                                     
-    <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-            <span class="caret"></span></button>
-        <ul class="dropdown-menu">
-            <li><a href="#">HTML</a></li>
-            <li><a href="#">CSS</a></li>
-            <li><a href="#">JavaScript</a></li>
-        </ul>
-    </div>
-</div>
-
 <table class="tini table table table-hover table-striped table-bordered" id="patrimonio-table"  >
     <thead>
     <td>@lang('ID')</td>
